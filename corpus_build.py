18b19c8683141c82d346aa61c2978df0ea6efe0f
Zack Cooper <zackjcooper@gmail.com>
2015-02-13 16:35:33 -0500

    testing against proper test data. All bare bones still in tact. Just adding features at this point. Rest is complete!!!

--- corpus_build.py
+++ corpus_build.py
@@ -5,82 +5,106 @@ import re
 
 
 file_types = {'.clj': 'clojure',
-'.cljs': 'clojure',
-'.clojure': 'clojure',
-'.edn': 'clojure',
-'.hs': 'haskell',
-'.lhs': 'haskell',
-'.ghc': 'haskell',
-'.java': 'java',
-'.class': 'java',
-'.js': 'javascript',
-'.javascript': 'javascript',
-'.ml': 'ocaml',
-'.mli': 'ocaml',
-'.ocaml': 'ocaml',
-'.perl': 'perl',
-'.php': 'php',
-'.phtml': 'php',
-'.php3': 'php',
-'.php4': 'php',
-'.php5': 'php',
-'.phps': 'php',
-'.python3': 'python',
-'.python2': 'python',
-'.py': 'python',
-'.jruby': 'ruby',
-'.scala': 'scala',
-'.racket': 'scheme',
-'.scm': 'scheme',
-'.ss': 'scheme',
-'.clojure': 'clojure'}
+              '.cljs': 'clojure',
+              '.clojure': 'clojure',
+              '.edn': 'clojure',
+              '.hs': 'haskell',
+              '.lhs': 'haskell',
+              '.ghc': 'haskell',
+              '.java': 'java',
+              '.class': 'java',
+              '.js': 'javascript',
+              '.javascript': 'javascript',
+              '.ml': 'ocaml',
+              '.mli': 'ocaml',
+              '.ocaml': 'ocaml',
+              '.perl': 'perl',
+              '.php': 'php',
+              '.phtml': 'php',
+              '.php3': 'php',
+              '.php4': 'php',
+              '.php5': 'php',
+              '.phps': 'php',
+              '.python3': 'python',
+              '.python2': 'python',
+              '.py': 'python',
+              '.jruby': 'ruby',
+              '.scala': 'scala',
+              '.racket': 'scheme',
+              '.scm': 'scheme',
+              '.ss': 'scheme',
+              '.clojure': 'clojure'}
 
 hit_num = {"clojure": "1",
-"haskell": "2",
-"java": "3",
-"javascript": "4",
-"ocaml": "5",
-"perl": "6",
-"php": "7",
-"python": "8",
-"ruby": "9",
-"scala": "10",
-"scheme": "11"}
+           "haskell": "2",
+           "java": "3",
+           "javascript": "4",
+           "ocaml": "5",
+           "perl": "6",
+           "php": "7",
+           "python": "8",
+           "ruby": "9",
+           "scala": "10",
+           "scheme": "11"}
 
-raw_file_list = [filename
-                 for filename in glob.iglob(os.path.join('corpus/bench',
-                                                         '*', '*'))
+raw_file_list = [filename for filename in
+                 glob.iglob(os.path.join('corpus/bench', '*', '*'))
                  if os.path.splitext(filename)[1] in file_types.keys()]
+
+feature_list = [('parent_count', "[()]"),
+                ('double_colon', "::"),
+                ('let_exists', r"\blet\b"),
+                ('less_minus', "((\<\-))"),
+                ('paren_star', "(\(\*|\*\))"),
+                ('def_exists', r"\bdef\b"),
+                ]
 
 
 class Corpus():
 
+    def __init__(self, file_list=[]):
+        self.file_list = file_list
 
     def read_process_file(self, file_name):
         with open(file_name) as f:
             return f.read()
 
-    def parenthesis_count(self, a_string):
-        return len(re.findall(r'[()]', a_string)) / len(a_string)
+    def build_dataframe(self, snippet=True):
+        raw_text = [self.read_process_file(file) for file in self.file_list]
 
-    def build_dataframe(self):
-        a_dataframe = pd.DataFrame([file_types[os.path.splitext(file)[1]]
-                                    for file
-                                    in raw_file_list], columns=['file_type'])
-        a_dataframe['hit_num'] = a_dataframe['file_type'].map(hit_num)
-        raw_text = [self.read_process_file(file) for file in raw_file_list]
-        a_dataframe['raw_text'] = raw_text
+        if snippet:
+            a_dataframe = pd.DataFrame(raw_text, columns=['raw_text'])
+        else:
+            a_dataframe = pd.DataFrame([file_types[os.path.splitext(file)[1]]
+                                        for file in self.file_list],
+                                       columns=['file_type'])
+
+            a_dataframe['hit_num'] = a_dataframe['file_type'].map(hit_num)
+            a_dataframe['raw_text'] = raw_text
 
         return a_dataframe
 
-    def feature_breakout(self, a_dataframe):
+    def feat_lookup(self, regex, a_dataframe):
+        return [(len(re.findall(regex, a_row))/len(a_row))
+                for a_row in a_dataframe['raw_text']]
 
-        a_dataframe['paren_count'] = [self.parenthesis_count(row)
-                                      for row in a_dataframe['raw_text']]
-        return a_dataframe
+    def feature_breakout(self, a_df):
 
+        for name, regex in feature_list:
+            a_df[name] = self.feat_lookup(regex, a_df)
 
-    def compl_df_build(self):
-        corpus = self.build_dataframe()
-        corpus = self.feature_breakout(corpus)
-        return corpus
+        return a_df
+
+    def compl_df_build(self, snippet=True):
+        a_df = self.build_dataframe(snippet)
+        a_df = self.feature_breakout(a_df)
+        cleaned_df = self.clean_df(a_df, snippet)
+        return cleaned_df
+
+    def clean_df(self, a_df, snippet):
+        cleaned_df = a_df.drop(['raw_text'], axis=1)
+
+        if not snippet:
+            cleaned_df = cleaned_df.drop(['file_type'], axis=1)
+
+        return cleaned_df
