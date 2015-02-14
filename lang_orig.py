75272fb45925604a1ae7a177dcf86cca4959b799
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-02-14 17:42:05 -0500

    testing with new files

--- lang_orig.py
+++ lang_orig.py
@@ -2,44 +2,47 @@ import os
 import os.path
 import pandas as pd
 import nltk
+import re
 from textblob import TextBlob as tb
 from sklearn import metrics
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction import DictVectorizer
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
-def read_train_files():
+
+def read_files(dir_path):
     file_list = []
-    for root, dirs, files in os.walk("./train/bench"):
+    for root, dirs, files in os.walk(dir_path):
         for name in files:
             file_list.append(os.path.join(root, name))
         for name in dirs:
             file_list.append((os.path.join(root, name)))
     return file_list
 
-def all_extensions(file_list):
-    return set([os.path.splitext(file)[1] for file in file_list])
 
 def get_extension(file):
     return os.path.splitext(file)[1]
 
+
 def get_language(ext):
     if ext in ['.clj', '.cljs', '.edn', '.clojure']:
         return 'Clojure'
-    elif ext in ['.hs', '.lhs','ghc']:
+    elif ext in ['.hs', '.lhs', 'ghc']:
         return 'Haskell'
     elif ext in ['.java', '.class', '.jar']:
         return 'Java'
     elif ext in ['.js', '.javascript']:
         return 'Javascript'
-    elif ext in ['.pl', '.pm', '.t', '.pod','.perl']:
+    elif ext in ['.pl', '.pm', '.t', '.pod', '.perl']:
         return 'Perl'
     elif ext in ['.php', '.phtml', '.php4', '.php3', '.php5', '.phps']:
         return 'PHP'
-    elif ext in ['.ocaml','.ml']:
+    elif ext in ['.ocaml', '.ml']:
         return 'Ocaml'
     elif ext in ['.py', '.pyw', '.pyc', '.pyo', '.pyd', '.python3']:
         return "Python"
@@ -47,39 +50,56 @@ def get_language(ext):
         return "Ruby"
     elif ext in ['.scala']:
         return 'Scala'
-    elif ext in ['.scm', '.ss','.racket']:
+    elif ext in ['.scm', '.ss', '.racket']:
         return "Scheme"
     elif ext in ['.tcl']:
         return "Tcl"
     return None
 
 def read_train_data():
-    files = read_train_files()
-    main_list=[]
+    files = read_files("./train/bench")
+    main_list = []
     for file in files:
         ext = get_extension(file)
         lang = get_language(ext)
         if lang != None:
             file_lang = []
-            with open(file,errors="surrogateescape") as in_file:
+            with open(file, errors="surrogateescape") as in_file:
                 texto = in_file.read()
-            main_list.append([texto,lang])
-    return main_list
+            main_list.append([texto, lang])
+    datadf = pd.DataFrame(main_list, columns = ['Code', 'Language'])
+    return datadf
+
+def read_test_data():
+    files = read_files("./test")
+    main_list = []
+    for file in files:
+            with open(file, errors="surrogateescape") as in_file:
+                texto = in_file.read()
+            file_name = re.findall(r'\d+', file)
+            main_list.append([texto,file_name[0]])
+    file_content = pd.DataFrame(main_list, columns=['Code', 'Filename'])
+    file_info = pd.read_csv('./test.csv', dtype={'Filename': str, 'Language': str})
+    datadf = pd.merge(file_info, file_content, on=['Filename'])
+    del datadf['Filename']
+    return datadf
 
 def join_all_code(content):
-    all_code = []
-    for ind, row in content.iterrows():
-        all_code.append(row['Code'])
-    return ''.join(all_code)
+    all_content = [row["Code"] for ind, row in content.iterrows()]
+    return ' '.join(all_content)
+
 
 def tokenize(content):
-    return nltk.word_tokenize(content)
+    tokens = nltk.word_tokenize(content)
+    return ' '.join(tokens)
+
 
 def word_freq(word, code):
     if len(code.words) == 0:
         return 0
     else:
         return code.word_counts[word] / len(code.words)
+
 
 def print_word_score(sorted_list):
     i=0
@@ -89,54 +109,116 @@ def print_word_score(sorted_list):
         if i == 10:
             break
 
-docu = read_train_data()
-datadf = pd.DataFrame(docu, columns = ['Code', 'Language'])
-#print(datadf.head())
-#print(datadf.shape)
-#print(datadf['Language'].value_counts())
-all_code = join_all_code(datadf)
-tokens = tokenize(all_code)
-all_tokens = ' '.join(tokens)
-token_blob = tb(all_tokens)
-scores = {word: word_freq(word,token_blob) for word in token_blob.words}
-sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+def return_tokenized_data(datadf):
+    all_code = join_all_code(datadf)
+    all_tokens = tokenize(all_code)
+    return tb(all_tokens)
 
-print_word_score(sorted_words)
 
-exclude_list = ['i','n','0','1','a']
-#Interesting find: the words "from",'m','on' should not be excluded. Performance lowers"
-final = {}
-for word, score in sorted_words:
-       if score > 0.001 and word not in exclude_list:
+def calculate_scores(token_blob):
+    scores = {word: word_freq(word,token_blob) for word in token_blob.words}
+    return sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+def select_features(word_list):
+    exclude_list = ['i','n','0','1','a']
+    final = {}
+    counter = 0
+    for word, score in word_list:
+        if word not in exclude_list:
            final[word] = round(score,5)
+        counter += 1
+        if counter == 200:
+            break
+    print("features#", len(final))
+    return final
 
-super_final = []
+def select_features_new_test(word_list, final):
+    final_list = {}
+    for key in final:
+        found = 0
+        for word, score in word_list:
+            if word == key:
+                found = score
+                break
+        final_list[key] = round(found,5)
+    return final_list
 
-for ind, row in datadf.iterrows():
+
+def create_vectors(final,data):
+    super_final = []
+    for ind, row in data.iterrows():
         mydict = {}
         for word, score in final.items():
             if row['Code'].find(word) != -1:
                 mydict[word] = score
         super_final.append(mydict)
+    vec = DictVectorizer()
+    return vec.fit_transform(super_final).toarray()
 
-from sklearn.feature_extraction import DictVectorizer
-vec = DictVectorizer()
-new_x = vec.fit_transform(super_final).toarray()
-new_x
+def create_vectors1(final,data):
+    super_final = []
+    for ind, row in data.iterrows():
+        mydict = {}
+        for word, score in final.items():
+            if row['Code'].find(word) != -1:
+                mydict[word] = score
+            else:
+                mydict[word] = 0
+        super_final.append(mydict)
+    vec = DictVectorizer()
+    return vec.fit_transform(super_final).toarray()
 
-x_train, x_test, y_train, y_test = train_test_split(new_x, datadf['Language'].values, test_size=0.4, random_state=0)
-clf = BernoulliNB()
-clf = clf.fit(x_train, y_train)
-predicted = clf.predict(x_test)
-print(metrics.classification_report(y_test, predicted))
-print(metrics.confusion_matrix(y_test, predicted))
-print(metrics.f1_score(y_test, predicted))
-scores = cross_val_score(clf, new_x,datadf['Language'].values, cv=5)
+def test_model(x,y,model):
+    if model == 'tree':
+        clf = tree.DecisionTreeClassifier()
+    elif model == 'bernoulli':
+        clf = BernoulliNB()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+    clf = clf.fit(x_train, y_train)
+    predicted = clf.predict(x_test)
+    print('predicted',predicted)
+    print(metrics.classification_report(y_test, predicted))
+    print(metrics.f1_score(y_test, predicted))
+    scores = cross_val_score(clf, x, y, cv=5)
+    return scores, clf
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(x_train, y_train)
-predicted = clf.predict(x_test)
-print(metrics.classification_report(y_test, predicted))
-print(metrics.confusion_matrix(y_test, predicted))
-print(metrics.f1_score(y_test, predicted))
-scores = cross_val_score(clf, new_x,datadf['Language'].values, cv=5)
+
+def test_new_models(data, final_words, clf):
+    new_target = data['Language'].values
+    token_blob = return_tokenized_data(data)
+    word_list = calculate_scores(token_blob)
+    final = select_features_new_test(word_list, final_words)
+    print('elemfinal',final)
+    print('final_words',final_words)
+    #vec = DictVectorizer()
+    #new_x =  vec.fit_transform(final).toarray()
+
+    new_x =  create_vectors1(final,data)
+    print('subzero',len(new_x[0]))
+    print('new',new_x)
+    predicted = clf.predict(new_x)
+    for pp in range(len(predicted)):
+        print('predicted',predicted[pp],new_target[pp])
+    print(new_target)
+
+    #print(metrics.classification_report(new_target, predicted))
+    #print(metrics.f1_score(new_target, predicted))
+    #scores = cross_val_score(clf, new_x, new_target)
+
+if __name__ == '__main__':
+    datadf = read_train_data()
+    target = datadf['Language'].values
+    read_test_data()
+    token_blob = return_tokenized_data(datadf)
+    word_list = calculate_scores(token_blob)
+    #print(word_list)
+    final_words = select_features(word_list)
+    print("final wordsoriginal", final_words)
+    new_x = create_vectors(final_words,datadf)
+    print('subzeroorig',len(new_x[0]))
+    print("newxorig",new_x)
+    #print(final_words)
+    bernoulli_score = test_model(new_x, target, 'bernoulli')
+    tree_score, clf_model = test_model(new_x, target, 'tree')
+    test_data = read_test_data()
+    test_new_models(test_data, final_words, clf_model)
