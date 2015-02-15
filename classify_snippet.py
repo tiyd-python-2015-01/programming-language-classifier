ba0735b2071c1d885185e6efff9cbc15aebc5d42
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-02-15 16:19:36 -0500

    executable ready to predict code passed as an argument

--- classify_snippet.py
+++ classify_snippet.py
@@ -13,6 +13,7 @@ from sklearn import tree
 import lang_orig as lg
 import sys
 
+
 def select_features_new_test(word_list, final):
         final_list = {}
         for key in final:
@@ -21,22 +22,25 @@ def select_features_new_test(word_list, final):
                 if word == key:
                         found = score
                         break
-            final_list[key] = round(found,5)
+            final_list[key] = round(found, 5)
         return final_list
 
 
 def setup_data():
-    cls = pickle.load( open( "model.p", "rb" ) )
-    final_features = pickle.load( open( "features.p", "rb" ) )
+    cls = pickle.load(open("model.p", "rb"))
+    final_features = pickle.load(open("features.p", "rb"))
     return final_features, cls
+
 
 def return_tokens(code):
     tokens = nltk.word_tokenize(code)
     return tb(' '.join(tokens))
 
+
 def create_vectors_new_test(features):
     vec = DictVectorizer()
     return vec.fit_transform(features).toarray()
+
 
 def predict(code, model, final_features):
     token_blob = return_tokens(code)
@@ -47,16 +51,19 @@ def predict(code, model, final_features):
     predicted_probs = model.predict_proba(new_x)
     return predicted, predicted_probs
 
+
 def print_probabilities(probas, model):
     print("Probabilites for my prediction:")
     for ind in range(len(probas[0])):
-        print(model.classes_[ind],':',round(probas[0][ind],2))
+        print(model.classes_[ind], ':', round(probas[0][ind], 2))
+
 
 def read_code():
     file_name = sys.argv[1]
     myfile = open(file_name)
     myfile_content = myfile.read()
     return myfile_content
+
 
 model_features, model = setup_data()
 code = read_code()
