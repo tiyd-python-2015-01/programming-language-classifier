a1a7a6d1a8207a25d78d18adb80b9c7bc9220bf1
Bret Runestad <bret.runestad@gmail.com>
2015-02-15 14:28:01 -0500

    created guess_lang.py, needs work on accuracy as well as function

--- plc/classifier.py
+++ plc/classifier.py
@@ -15,3 +15,8 @@ def run_classifier(clf, x_train, x_test, y_train, y_test):
     print(metrics.classification_report(y_test, predicted))
     print(metrics.confusion_matrix(y_test, predicted))
     print(metrics.f1_score(y_test, predicted))
+
+def create_train(df, startx, columny):
+    x_train = df.loc[:, startx:]
+    y_train = df[columny]
+    return x_train, y_train
