284aca085ca9fc9914f8e6e1c18fb3f375f007ee
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-02-15 15:53:24 -0500

    Code to train the model

--- lang_orig.py
+++ lang_orig.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import pickle
 import pandas as pd
 import nltk
 import re
@@ -70,19 +71,6 @@ def read_train_data():
     datadf = pd.DataFrame(main_list, columns = ['Code', 'Language'])
     return datadf
 
-def read_test_data():
-    files = read_files("./test")
-    main_list = []
-    for file in files:
-            with open(file, errors="surrogateescape") as in_file:
-                texto = in_file.read()
-            file_name = re.findall(r'\d+', file)
-            main_list.append([texto,file_name[0]])
-    file_content = pd.DataFrame(main_list, columns=['Code', 'Filename'])
-    file_info = pd.read_csv('./test.csv', dtype={'Filename': str, 'Language': str})
-    datadf = pd.merge(file_info, file_content, on=['Filename'])
-    del datadf['Filename']
-    return datadf
 
 def join_all_code(content):
     all_content = [row["Code"] for ind, row in content.iterrows()]
@@ -101,14 +89,6 @@ def word_freq(word, code):
         return code.word_counts[word] / len(code.words)
 
 
-def print_word_score(sorted_list):
-    i=0
-    for word, score in sorted_list:
-        print(word,score)
-        i+=1
-        if i == 10:
-            break
-
 def return_tokenized_data(datadf):
     all_code = join_all_code(datadf)
     all_tokens = tokenize(all_code)
@@ -119,29 +99,19 @@ def calculate_scores(token_blob):
     scores = {word: word_freq(word,token_blob) for word in token_blob.words}
     return sorted(scores.items(), key=lambda x: x[1], reverse=True)
 
+
 def select_features(word_list):
-    exclude_list = ['i','n','0','1','a']
-    final = {}
+    exclude_list = ['l','6','d','x1','x2','5','j','m','w','i','e','g','4','v','n','0','1','a','x','n','y','b','2','c','s','3']
+    final={}
     counter = 0
     for word, score in word_list:
         if word not in exclude_list:
            final[word] = round(score,5)
         counter += 1
-        if counter == 200:
+        if counter == 160:
             break
     print("features#", len(final))
     return final
-
-def select_features_new_test(word_list, final):
-    final_list = {}
-    for key in final:
-        found = 0
-        for word, score in word_list:
-            if word == key:
-                found = score
-                break
-        final_list[key] = round(found,5)
-    return final_list
 
 
 def create_vectors(final,data):
@@ -172,17 +142,6 @@ def test_model(x,y,model):
     return scores, clf
 
 
-def test_new_models(data, final_words, clf):
-    new_target = data['Language'].values
-    token_blob = return_tokenized_data(data)
-    word_list = calculate_scores(token_blob)
-    final = select_features_new_test(word_list, final_words)
-    new_x =  create_vectors(final,data)
-    predicted = clf.predict(new_x)
-    for pp in range(len(predicted)):
-        print('predicted',predicted[pp],new_target[pp])
-
-
 def test_model(x,y,model):
     if model == 'tree':
         clf = tree.DecisionTreeClassifier()
@@ -191,7 +150,7 @@ def test_model(x,y,model):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
     clf = clf.fit(x_train, y_train)
     predicted = clf.predict(x_test)
-    print('predicted',predicted)
+    #print('predicted',predicted)
     print(metrics.classification_report(y_test, predicted))
     print(metrics.f1_score(y_test, predicted))
     scores = cross_val_score(clf, x, y, cv=5)
@@ -201,13 +160,12 @@ def test_model(x,y,model):
 if __name__ == '__main__':
     datadf = read_train_data()
     target = datadf['Language'].values
-    print(datadf.groupby('Language').count())
-    read_test_data()
     token_blob = return_tokenized_data(datadf)
     word_list = calculate_scores(token_blob)
     final_words = select_features(word_list)
+    #print("word_list for model", final_words)
     new_x = create_vectors(final_words,datadf)
     bernoulli_score = test_model(new_x, target, 'bernoulli')
     tree_score, clf_model = test_model(new_x, target, 'tree')
-    test_data = read_test_data()
-    test_new_models(test_data, final_words, clf_model)
+    pickle.dump( clf_model, open( "model.p", "wb" ) )
+    pickle.dump( final_words, open( "features.p", "wb" ) )
