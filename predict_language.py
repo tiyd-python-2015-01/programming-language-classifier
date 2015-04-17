891c8e023bb3e996d77300bc24074e3a3d3101d0
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-04-17 14:52:45 -0400

    Refactored the making predictions functions.

--- predict_language.py
+++ predict_language.py
@@ -6,6 +6,9 @@ import sys
 import pickle
 
 
+languages =["Clojure", "Haskell", "Java", "JavaScript", "Ocaml", "Perl",
+        "Php", "Python", "Ruby", "Scala", "Scheme"]
+
 def make_temp_df(text):
     """Creates a basic temporary Dataframe."""
     temp_df = pd.DataFrame({"Text": text})
@@ -15,24 +18,19 @@ def make_temp_df(text):
     return temp_df
 
 
-def present_percent(probs):
-    probs = probs.tolist()
-    langs =["Clojure", "Haskell", "Java", "JavaScript", "Ocaml", "Perl",
-            "Php", "Python", "Ruby", "Scala", "Scheme"]
-    prob_dict = {}
-    counter = 0
-    for prob in probs[0]:
-        prob_dict[langs[counter]] = prob
-        counter += 1
-    sorted_dict = Counter(prob_dict)
-    top_three = sorted_dict.most_common(3)
-    first_lang, first_num = top_three[0]
-    second_lang, second_num = second = top_three[1]
-    third_lang, third_num = top_three[2]
+def present_percent(probabilites):
+    tuples = list(zip(languages, probabilites.tolist()[0]))
+    sorted_list = sorted(tuples, key=lambda x: x[1], reverse=True)
+    first_lang, first_num = sorted_list[0]
+    second_lang, second_num = sorted_list[1]
+    third_lang, third_num = sorted_list[2]
     print("Most likely languages...\n",
-          "1.{}: {}%\n".format(first_lang, round(first_num, 3) * 100),
-          "2.{}: {}%\n".format(second_lang, round(second_num, 3) * 100),
-          "3.{}: {}%".format(third_lang, round(third_num, 3) * 100))
+          "1. {}: {}%\n".format(first_lang, round(first_num * 100, 1)),
+          "2. {}: {}%\n".format(second_lang, round(second_num * 100, 1)),
+          "3. {}: {}%".format(third_lang, round(third_num * 100, 1)))
+    return sorted_list
+
+
 
 
 def make_prediction(test_file):
@@ -48,6 +46,17 @@ def make_prediction(test_file):
     print("I predict the language is: {}".format(prediction[0]))
     present_percent(probability)
 
+
+def make_prediction_from_text(text):
+    text_list = [text]
+    temp_df = make_temp_df(text_list)
+    file = open("language_detector.pkl",'rb')
+    classifier = pickle.load(file)
+    prediction = classifier.predict(temp_df.loc[0::,'Object':"php"])
+    probability = classifier.predict_proba(temp_df.loc[0::,'Object':"php"])
+    print("I predict the language is: {}".format(prediction[0]))
+    present_percent(probability)
+
 if __name__ == '__main__':
-    test_file = sys.argv[1]
-    make_prediction(test_file)
+    text = sys.argv[1]
+    make_prediction(text)
